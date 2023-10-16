1fcf83decc50a7d8ab3e84c6757eb4040a2695b6
radiocom <radiocom@qq.com>
2023-10-16 22:27:17 +0800

    Finish all tests

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,7 +12,6 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::borrow::Cow;
 
@@ -49,6 +48,8 @@ mod tests {
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(_) => Ok(()),
+            Cow::Owned(_) => Err("Expected borrowed value"),
         }
     }
 
@@ -61,6 +62,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(_) => Err("Expected borrowed value"),
+            Cow::Owned(_) => Ok(()),
         }
     }
 
@@ -73,6 +76,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(_) => Err("Expected borrowed value"),
+            Cow::Owned(_) => Ok(()),
         }
     }
 }
